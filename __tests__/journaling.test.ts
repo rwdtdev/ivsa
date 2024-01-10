1867aa9339ce76f841ba66c3b8606c27ca68c9b1
mzhukovPi <mzhukov31415dev2@gmail.com>
2024-01-10 18:24:06 +0300

    Первый пуш, добавил перечень тестов и пустышку для описания (readme в корне проекта)

---
+++ __tests__/journaling.test.ts
@@ -0,0 +1,60 @@
+describe('Testing internal operations journaling', () => {
+
+//  const VALID_PAYLOADS_TO_STORE = [];
+//  const SUBSYSTEM_IDS = [];
+//  const USER_IDS = [];
+
+  describe('store a journal entry', () => {
+
+    describe('input validity checks', () => {
+      it('valid input', () => {
+        // ...
+        expect(true).toBeTruthy();
+      });
+
+      it('missing subsystem id', () => {
+        // ...
+        expect(true).toBeTruthy();
+      });
+
+      it('missing user id', () => {
+        // ...
+        expect(true).toBeTruthy();
+      });
+
+      it('missing or malformed payload', () => {
+        // ...
+        expect(true).toBeTruthy();
+      });
+
+    });
+
+    it('store an entry', () => {
+      // store an entry
+      // retrieve the entry stored
+      // check if stored payloads and metadata match the retrieved
+      expect(true).toBeTruthy();
+    });
+
+  });
+
+  describe('retrieve stored entries from journal', () => {
+
+    it('retrieve a single entry', () => {
+      // ...
+      expect(true).toBeTruthy();
+    });
+
+    it('retrieve a non-existant entry', () => {
+      // ...
+      expect(true).toBeTruthy();
+    });
+
+    it('retrieve multiple entries', () => {
+      // timestamps range, list of subsystem ids, list of user ids
+      expect(true).toBeTruthy();
+    });
+
+  });
+
+});

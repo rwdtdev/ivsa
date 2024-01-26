5bccd0d4f5e74ab46210462677e4a66dce2c75eb
Sergey Nikulin <snikulin@gmail.com>
2024-01-26 17:38:25 +0300

    show IVA server info it's temp. solution for testing only

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,12 @@
-export default function page() {
-  return <></>;
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { getServerStatus } from "@/server/services/iva";
+
+export default async function page() {
+  const ivaServerStatus = await getServerStatus();
+  return <>
+    <h1>Admin</h1>
+    <ScrollArea className='h-[90vh] rounded-md border'>
+      <pre>IVA Server Status: {JSON.stringify(ivaServerStatus, null, '\t')}</pre>
+    </ScrollArea>
+  </>;
 }

761d9ebbfa0b87ddb9158f0a1e50a4674094e720
a.grachev <a.grachev@rwdt.ru>
2024-07-26 09:10:24 +0300

    fix: Inventories dateTime & as_vi

--- app/events/[eventId]/page.tsx
+++ app/events/[eventId]/page.tsx
@@ -12,7 +12,7 @@ import { P } from '@/components/ui/typography/p';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { UserRoles } from '@/constants/mappings/prisma-enums';
-import { DATETIME_FORMAT, DATE_FORMAT } from '@/constants/date';
+import { DATE_FORMAT } from '@/constants/date';
 import { BriefingStatusBadge, EventStatusBadge } from '@/components/event-status-badge';
 
 import { ScrollArea } from '@/components/ui/scroll-area';
@@ -111,11 +111,11 @@ export default function EventPage() {
                   </div>
                   <P className='text-sm'>
                     <span className='font-semibold'>Дата начала:</span>{' '}
-                    {moment(event.startAt).format(DATETIME_FORMAT)}
+                    {moment(event.startAt).format(DATE_FORMAT)}
                   </P>
                   <P className='text-sm'>
                     <span className='font-semibold'>Дата окончания:</span>{' '}
-                    {moment(event.endAt).format(DATETIME_FORMAT)}
+                    {moment(event.endAt).format(DATE_FORMAT)}
                   </P>
                   <P className='text-sm'>
                     <span className='font-semibold'>Балансовая единица:</span>{' '}
